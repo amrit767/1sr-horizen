5214749156962d4d1a8847db63236977b23cdd74
amrit sood <soodamrit90@gmail.com>
2026-06-10 22:40:45 +0530

    update

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 const maxNum = 100;
 const minNum = 1;
-let randomNum = Math.floor(Math.random() * (maxNum) - (minNum)) + minNum;
 
-let attempts = 0 ;
-let guess ;
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+
+let attempts = 0;
+let guess;
 let running = true;
 
-while(running){
-    guess = window.prompt(`enter number`);
+while (running) {
+    guess = window.prompt(`enter a number between ${minNum} and ${maxNum} `);
     guess = Number(guess);
-    console.log(typeof guess, guess);
-    running:false;
+
+    if (isNaN(guess)) {
+        window.alert("Enter a valid Number");
+    }
+
+    else if (guess < minNum || guess > maxNum) {
+        window.alert("Enter a valid Number");
+    }
+
+    else {
+        attempts++;
+
+        if (guess > answer) {
+            window.alert("YOUR ANSWER IS TOO big !! TRY AGAIN");
+        }
+        else if (guess < answer) {
+            window.alert("YOUR ANSWER IS TOO LOW !! TRY AGAIN");
+        }
+        else {
+            window.alert(
+                `CONGRATS, THE ANSWER WAS ${answer}, IT TOOK YOU ${attempts} TO GUESS CORRECTLY`
+            );
+            running = false;
+        }
+    }
 }
