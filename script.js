633e8bada40cd7fae31bcc523fab2efc82429141
amrit sood <soodamrit90@gmail.com>
2026-06-13 04:24:18 +0530

    ipdate

--- script.js
+++ script.js
@@ -3,36 +3,91 @@ const minNum = 1;
 
 const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 
-let attempts = 0;
-let guess;
-let running = true;
+const input = document.querySelector(".myInput");
+const button = document.querySelector(".myBtn");
+const stdout = document.querySelector("#stdout");
+const conclusion = document.querySelector("#conclusion-label");
+const history = document.querySelector{ "#history"}
 
-while (running) {
-    guess = window.prompt(`enter a number between ${minNum} and ${maxNum} `);
-    guess = Number(guess);
+let attempts = 0;
+let guesses = [];
+
+button.onclick = function () {
+    let guess = Number(input.value);
+
+    if (input.value === "") {
+        stdout.textContent = "STDOUT: NO INPUT";
+        conclusion.textContent = "PLEASE ENTER A NUMBER";
+        return;
+    }
 
     if (isNaN(guess)) {
-        window.alert("Enter a valid Number");
+        stdout.textContent = "STDOUT: INVALID INPUT";
+        conclusion.textContent = "ENTER A VALID NUMBER";
+        return;
     }
 
-    else if (guess < minNum || guess > maxNum) {
-        window.alert("Enter a valid Number");
+    if (guess < minNum || guess > maxNum) {
+        stdout.textContent = "STDOUT: OUT OF RANGE";
+        conclusion.textContent = ` ENTER A NUMBER BETWEEN ${minNum} AND ${maxNum} `;
+        input.value = "";
+        return;
+
     }
 
+
+    if (guess > answer) {
+        stdout.textContent = `STDOUT: ATTEMPT ${attempts}`;
+        conclusion.textContent = `${guess} IS TOO HIGH`;
+        guesses.push(
+            `attempts ${attempts}: ${guess} > TOO HIGH`
+        );
+        history.innerHTML = guesses.join("<br>");
+        input.value = "";
+
+    }
+    else if (guess < answer) {
+
+        stdout.textContent = `STDOUT ATTEMPT ${attempts}`;
+        conclusion.textContent = `${guess} IS TOO HIGH`;
+        guesses.push(
+            `attempts ${attempts}: ${guess} > TOO LOW`
+        );
+        history.innerHTML = guesses.join("<br>");
+        input.value = "";
+    }
     else {
-        attempts++;
 
-        if (guess > answer) {
-            window.alert("YOUR ANSWER IS TOO big !! TRY AGAIN");
-        }
-        else if (guess < answer) {
-            window.alert("YOUR ANSWER IS TOO LOW !! TRY AGAIN");
-        }
-        else {
-            window.alert(
-                `CONGRATS, THE ANSWER WAS ${answer}, IT TOOK YOU ${attempts} TO GUESS CORRECTLY`
-            );
-            running = false;
-        }
+        stdout.textContent = `STDOUT ATTEMPT ${attempts}`;
+        conclusion.textContent = `${guess} IS THE CORRECT ANSWER`;
+        guesses.push(
+            `attempts ${attempts}: ${guess} > CORRECT`
+        );
+        history.innerHTML = guesses.join("<br>");
+        setTimeout(newGame, 3000);
+
+
+
     }
+};
+
+function newGame(){
+    answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+    attempts = 0 ;
+    guesses = [] ;
+    history.innerHTML = "NEW ROUND STARTED" ;
+    stdout.textContent = "STDOUT: WAITING...";
+    conclusion.textContent = "CONCLUSION OF YOUR GUESS";
+    input.value = "";
+    input.disabled = false;
+    button.disabled = false;
+
 }
+
+input.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        button.click();
+    }
+});
+
+console.log("ANSWER = " , answer);
